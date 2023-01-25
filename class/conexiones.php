6c197137ec0ee88cf682a9a63e739c33ee0d008d
Juan Basgall <juanangelbasgall@hotmail.com>
2023-01-25 00:42:06 -0300

    Inicio de clase conexiones, pruebas con fechas y organizacion de obtención de datos

---
+++ class/conexiones.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of conexiones
+ *
+ * @author Hans
+ */
+class conexiones {
+    public $id;
+    public $ip;
+    public $agent;
+    public $navegador;
+    public $dispositivo;
+    public $so;
+    public $fecha;
+    
+    public function __construct($id = null){
+        $this->id = $id++;
+        $this->ip = getenv("REMOTE_ADDR");
+        $this->agent = getenv("HTTP_USER_AGENT");
+        if( preg_match('/MSIE (\d+\.\d+);/', $this->agent) ) {
+          $this->navegador = "Internet Explorer";
+        } else if (preg_match('/Chrome[\/\s](\d+\.\d+)/', $this->agent) ) {
+          $this->navegador = "Chrome";
+        } else if (preg_match('/Edge\/\d+/', $this->agent) ) {
+          $this->navegador = "Edge";
+        } else if ( preg_match('/Firefox[\/\s](\d+\.\d+)/', $this->agent) ) {
+          $this->navegador = "Firefox";
+        } else if ( preg_match('/OPR[\/\s](\d+\.\d+)/', $this->agent) ) {
+          $this->navegador = "Opera";
+        } else if (preg_match('/Safari[\/\s](\d+\.\d+)/', $this->agent) ) {
+          $this->navegador = "Safari";
+        }
+        
+        $iphone = is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'iphone'));
+        $android = is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'android'));
+        $palmpre = is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'webos'));
+        $blackberry = is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'blackberry'));
+        $ipod = is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'ipod'));
+        $ipad = is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'ipad'));
+
+        if ($iphone){
+            $this->dispositivo = "iPhone";
+            $this->so = "iOS";
+        }else if ($android){
+            $this->dispositivo = "Android";
+            $this->so = "Android";
+        }else if ($palmpre){
+            $this->dispositivo = "Web OS";
+            $this->so = "Web OS";
+        }else if ($blackberry){
+            $this->dispositivo = "BlackBerry";
+            $this->so = "BlackBerry OS";
+        }else if ($ipod){
+            $this->dispositivo = "iPod";
+            $this->so = "iOS";
+        }else if ($ipad){
+            $this->dispositivo = "iPad";
+            $this->so = "iPad OS";
+        }else{
+            $this->dispositivo = "Computer";
+            $win = is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'win'));
+            $linux = is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'linux'));
+            $mac = is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'mac'));
+            if ($win){
+                $this->so = "Windows";
+            }else if ($linux){
+                $this->so = "Linux";
+            }else{
+                $this->so ="MacOS";
+            }
+        }
+        date_default_timezone_set("America/Argentina/Buenos_Aires");
+        $this->fecha = date("D d-M-Y  H:i:s");
+    }
+}
+
+$nuevaConexion = new conexiones();
+echo "<pre>";
+print_r($nuevaConexion);
+echo "</pre>";
