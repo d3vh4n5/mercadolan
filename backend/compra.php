b9cd8773d8926bef2b9e242f2da8d4f405895869
Juan Basgall <juanangelbasgall@hotmail.com>
2023-04-04 13:25:00 -0300

    Agregado pop up de respuesta a compra sin estar Log

--- backend/compra.php
+++ backend/compra.php
@@ -1,7 +1,31 @@
 <?php
+session_start();
 //require './views/compra.html';
-require './backend/views/compra.html';
 //require '../class/autoload.php';
 
 
+if (isset($_POST['action'])){
+    
+    //print_r($_SESSION);
+    //die();
+    if (!isset($_SESSION['session1']['nombre'])){
+        $mensaje = "<div class='alertaLog'>
+                <h3>⚠️ No estás logeado<br></h3>
+                <br>
+                <p>Por favor, logéate para continuar con el proceso de compra.</p>
+                <br>
+                <button name='back' onclick='history.back()' action='back'>OK</button>
+              </div>";
+        echo $mensaje;
+    } else {
+        echo "<p style='background: orange; '>Flow en construcción</p>";
+    }
+    
+}
+
+
+
+
+
+require './backend/views/compra.html';
 $basepath = $_SERVER['SCRIPT_NAME'];
