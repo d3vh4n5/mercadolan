079278b077897e008b0024ca7f512ba97ad7025f
Juan Basgall <juanangelbasgall@hotmail.com>
2023-04-15 15:27:11 -0300

    agregado pequeño js para autoestcalar la lista d eproductos

---
+++ assets/js/tablaproductos.js
@@ -0,0 +1,18 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+window.onresize = function() { // Detectar cuando se cambia el tamaño de la ventana
+  var width = window.innerWidth; // Obtener el ancho de la ventana
+  if (width < 1640){
+    var zoom = width / 1640; // Calcular el valor del zoom (800 es el ancho de la página original)
+    document.body.style.zoom = zoom; // Establecer el valor del zoom en el cuerpo de la página     
+  }
+}
+
+var width = window.innerWidth; // Obtener el ancho de la ventana
+if (width < 1640){
+    var zoom = width / 1640; // Calcular el valor del zoom (800 es el ancho de la página original)
+    document.body.style.zoom = zoom; // Establecer el valor del zoom en el cuerpo de la página     
+}
